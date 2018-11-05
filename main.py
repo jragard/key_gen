f0cf0937eba9b1a4c9a4d11f2ce4ab92452cd68e
Ryan <jragard@gmail.com>
2018-11-05 11:51:50 -0500

    fixed both functions to be walled off and separate, now a key is not returned by generate_key unless it is first validated by the rules defined in validate_key

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import secrets
 
 
-def generate_key():
-    """
-    Generates a random 32 character string, and simulates
-    storing this string in a database by writing it to db.txt
-    """
-
-    key = secrets.token_hex(16)
-    f = open('db.txt', 'a')
-    f.write(key + '\n')
-
-
-generate_key()
-
-
 def validate_key(key):
     """
-    Takes a key string as an argument, and checks our
-    'database' db.txt to see if the key is stored there.
-    If so, returns True, if not, returns False
+    Takes a key string as an argument, and checks if the
+    cumulative score as created by the ord function on each character
+    equals 2118. If so, returns True, if not, returns False
     """
+    score = 0
 
-    keys = open('db.txt', 'r').read()
-    if key in keys:
+    for char in key:
+        score += ord(char)
+
+    if score == 2118:
         return True
     else:
         return False
 
 
-print(validate_key('a5c4ebeddd3f50faf4fb1d7497476e37'))
+def generate_key():
+    """
+    Generates a random 32 character string, and uses the validate_key
+    function to check if the key is valid.  If not valid, it keeps
+    attempting to generate keys until it comes up with a valid key
+    """
+
+    while True:
+        key = secrets.token_hex(16)
+        if validate_key(key):
+            return key
+
+
+k = generate_key()
+
+print(k)
+print(validate_key(k))
